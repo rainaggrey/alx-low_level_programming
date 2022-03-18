ee719bc3c99405dd47dec154ecb1080e1f19418c
rainaggrey <raindolfaggrey8@gmail.com>
2022-03-18 17:23:00 +0000

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * @void: no parameter
+ *
+ * Description: prints string
+ * Return: 0 if true
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+
+	return 0;
+	
+}
